4cf563f683e99c16746d093207166030960aac5a
Huuhao1712 <nhhao21@apcs.fitus.edu.vn>
2024-04-12 01:16:58 +0700

    Create question and answer class

---
+++ RacingArenaClient/src/main/java/org/racingarena/client/object/QuestionNAnswer.java
@@ -0,0 +1,38 @@
+package org.racingarena.client.object;
+
+import java.util.Random;
+
+public class QuestionNAnswer {
+    private String question;
+    private int answer;
+    private int duration;
+    public QuestionNAnswer() {
+        this.answer = 0;
+        this.question = "N/A";
+        duration = 0;
+    }
+
+    public String getQuestion(){
+        return question;
+    }
+
+    public void setQuestion(String question){
+        this.question = question + " =";
+    }
+
+    public int getAnswer(){
+        return answer;
+    }
+
+    public void setAnswer(int answer){
+        this.answer = answer;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+}
