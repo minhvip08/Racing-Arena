f579ce531e10782850f9a308d7362802ec0a0e9a
Huuhao1712 <nhhao21@apcs.fitus.edu.vn>
2024-04-12 01:17:30 +0700

    Update UI for WaitingScreeen

--- RacingArenaClient/src/main/java/org/racingarena/client/screen/WaitingScreen.java
+++ RacingArenaClient/src/main/java/org/racingarena/client/screen/WaitingScreen.java
@@ -1,6 +1,14 @@
 package org.racingarena.client.screen;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.ScreenUtils;
 import org.racingarena.client.game.RacingArena;
 import org.racingarena.client.socket.Status;
@@ -9,9 +17,25 @@ import java.util.Objects;
 
 public class WaitingScreen implements Screen {
     final RacingArena game;
+    final Stage stage;
+    final Skin skin;
 
     public WaitingScreen(final RacingArena game) {
         this.game = game;
+        this.stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+        skin = new Skin(Gdx.files.classpath("orangepeelui/uiskin.json"));
+
+        Table rootTable = new Table(skin);
+        rootTable.setFillParent(true);
+
+        Label label = new Label("Waiting for other players to join...", skin, "title");
+
+        label.setAlignment(Align.center);
+
+        rootTable.add(label).grow();
+
+        stage.addActor(rootTable);
     }
 
     @Override
@@ -21,7 +45,10 @@ public class WaitingScreen implements Screen {
 
     @Override
     public void render(float v) {
-        ScreenUtils.clear(0, 0, 0, 1);
+        Gdx.gl.glClearColor(1.0f, 1.0f, 1.0f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
         if (Objects.equals(game.gamePlay.getStatus(), Status.CLIENT_READY)) {
             game.setScreen(new GameScreen(game));
         }
